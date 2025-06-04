429c39191fbc7aa0fda0901b269af3318ba6fba1
Mugishaa77 <Sallywanga2016@gmail.com>
2025-06-04 12:42:01 +0300

    Optimized index page for seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  
   return (
-    <>
-     
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
